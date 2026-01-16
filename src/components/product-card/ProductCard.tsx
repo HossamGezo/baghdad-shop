ccadaaf2b6b2859410bc0a88758d0e5e0ed7dce2
HossamGezo <ha2ghossam10@gmail.com>
2026-01-16 08:32:55 +0200

    feat(ui): create responsive product slider component and update product card

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -13,13 +13,19 @@ type ProductCardProps = {
 // --- ProductCard (Main Component)
 const ProductCard = (product: ProductCardProps) => {
   return (
-    <div className="product-card bg-white w-80 flex flex-col items-center p-4 rounded-md">
+    <div className="product-card bg-white min-w-[319.5px] flex flex-col items-center p-4 rounded-md">
       <div className="product-title-image-wrapper w-40 h-40 mb-5">
-        <img src={product.image} alt={product.title} className="w-full h-full object-contain"/>
+        <img
+          src={product.image}
+          alt={product.title}
+          className="w-full h-full object-contain"
+        />
       </div>
       <div className="product-card-title font-medium">{product.title}</div>
       <RatingAndViews rating={product.rating} reviews={product.reviews} />
-      <div className="product-card-price text-xl font-medium">USD {product.price}</div>
+      <div className="product-card-price text-xl font-medium">
+        USD {product.price}
+      </div>
     </div>
   );
 };
