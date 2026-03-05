bdf059478bf0ffcedb03e286851b6b44ca185cac
HossamGezo <ha2ghossam10@gmail.com>
2026-03-05 14:30:41 +0200

    feat: implement modular UI kit with generic input fields, password visibility, and flexible buttons

---
+++ src/components/custom-button/CustomButton.tsx
@@ -0,0 +1,36 @@
+// --- Libraries
+import { cn } from "@utils/cn";
+import Spinner from "@components/spinner/Spinner";
+
+// --- Types
+type CustomButtonProps = React.ComponentProps<"button"> & {
+  children: React.ReactNode;
+  isLoading?: boolean;
+};
+
+// --- Main Component
+const CustomButton = ({
+  children,
+  isLoading,
+  disabled,
+  className,
+  ...rest
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={isLoading || disabled}
+      className={cn(
+        "bg-warning shadow-standard font-medium text-primary py-1.5 rounded-md cursor-pointer transition-all duration-75 flex items-center justify-center select-none h-9",
+        isLoading || disabled
+          ? "opacity-50 cursor-not-allowed"
+          : "hover:bg-amber-400 active:scale-[0.99] active:bg-warning",
+        className,
+      )}
+      {...rest}
+    >
+      {isLoading ? <Spinner className="w-4 h-4" /> : children}
+    </button>
+  );
+};
+
+export default CustomButton;
--- src/components/inputs/InputField.tsx
+++ src/components/inputs/InputField.tsx
@@ -1,32 +1,38 @@
-// Libraries
-import { cn } from "@/utils/cn";
+// --- Libraries
+import { cn } from "@utils/cn";
 import type { Path, UseFormRegister, FieldValues } from "react-hook-form";
 
-// Types
+// --- Types
 type InputOptionType = "text" | "email" | "password";
 
-// Main Type
+// --- Main Type
 type InputFieldProps<T extends FieldValues> = {
   type: InputOptionType;
   name: Path<T>;
   register: UseFormRegister<T>;
+  label: string;
   error?: string;
 } & Omit<React.ComponentProps<"input">, "name">;
 
-// Main Component
-const CustomInput = <T extends FieldValues>({
+// --- Main Component
+const InputField = <T extends FieldValues>({
   type = "text",
   name,
   register,
+  label,
   error,
   className,
   ...props
 }: InputFieldProps<T>) => {
   const baseStyles =
-    "w-[325px] h-[45px] bg-white border-b-2 border-b-secondary/75 focus:border-b-primary outline-0 px-1.5 py-2 caret-primary rounded-sm placeholder:text-secondary/30 placeholder:text-sm";
+    "w-full sm:w-[325px] h-[45px] bg-white border border-primary/25 focus:border-primary outline-0 px-1.5 py-2 caret-primary rounded-sm placeholder:text-primary/50 placeholder:text-sm";
   return (
     <div className="flex flex-col gap-1">
+      <label htmlFor={name} className="mb-1.5 cursor-pointer text-primary">
+        {label}
+      </label>
       <input
+        id={name}
         type={type}
         {...props}
         {...register(name)}
@@ -36,4 +42,4 @@ const CustomInput = <T extends FieldValues>({
     </div>
   );
 };
-export default CustomInput;
+export default InputField;
