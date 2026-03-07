91f4a3541d26878a2606ae88bcb13e8d28113c72
HossamGezo <ha2ghossam10@gmail.com>
2026-03-07 15:18:35 +0200

    feat: implement product exclusion logic in related sections using excludeId

--- src/pages/product-details/ProductDetails.tsx
+++ src/pages/product-details/ProductDetails.tsx
@@ -1,4 +1,4 @@
-// --- Liraries
+// --- Libraries
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
@@ -17,10 +17,13 @@ import Spinner from "@components/spinner/Spinner";
 import ErrorHandler from "@components/error-handler/ErrorHandler";
 import { addToCart } from "@features/cart/cartSlice";
 import type { ProductType } from "@/types";
+import Mobiles from "@pages/home/mobiles/Mobiles";
+import Laptops from "@pages/home/laptops/Laptops";
+import SpecialOffers from "@pages/home/special-offers/SpecialOffers";
 
 // --- Main Component
 const ProductDetails = () => {
-  const params = useParams();
+  const { category, id } = useParams();
   const [count, setCount] = useState<number>(1);
 
   // --- RTK Custom Hooks
@@ -31,13 +34,11 @@ const ProductDetails = () => {
 
   // --- Fetch Data
   useEffect(() => {
-    dispatch(
-      fetchSingleProduct({ category: params.category!, id: params.id! }),
-    );
+    dispatch(fetchSingleProduct({ category: category!, id: id! }));
     return () => {
       dispatch(clearSingleProduct());
     };
-  }, [dispatch, params.category, params.id]);
+  }, [dispatch, category, id]);
 
   // --- Add To Cart Logic
   const addToCartFunc = (product: ProductType) => {
@@ -60,59 +61,68 @@ const ProductDetails = () => {
       )}
 
       {!loading && !error && singleProduct && (
-        <div className="bg-primary/10 my-5 sm:p-5 rounded-lg">
-          {/* --- Product Details Wrapper */}
-          <div className="bg-white shadow-strong lg:w-3/4 mx-auto grid grid-cols-4 p-5 rounded-md">
-            {/* Product Images */}
-            <ProductImages
-              images={singleProduct.images}
-              title={singleProduct.title}
-            />
+        <div className="mb-10">
+          <div className="bg-primary/10 my-5 sm:p-5 rounded-lg">
+            {/* --- Product Details Wrapper */}
+            <div className="bg-white shadow-strong lg:w-3/4 mx-auto grid grid-cols-4 p-5 rounded-md">
+              {/* Product Images */}
+              <ProductImages
+                images={singleProduct.images}
+                title={singleProduct.title}
+              />
 
-            {/* Card Details */}
-            <div className="col-span-4 md:col-span-2">
-              {/* Product Description */}
-              <div>
-                <h1 className="text-primary text-[22px] font-medium underline decoration-warning mb-2.5">
-                  {singleProduct.title}
-                </h1>
-                <p className="text-primary text-[14px] xl:text-base text-justify">
-                  {singleProduct.description}
-                </p>
+              {/* Card Details */}
+              <div className="col-span-4 md:col-span-2">
+                {/* Product Description */}
+                <div>
+                  <h1 className="text-primary text-[22px] font-medium underline decoration-warning mb-2.5">
+                    {singleProduct.title}
+                  </h1>
+                  <p className="text-primary text-[14px] xl:text-base text-justify">
+                    {singleProduct.description}
+                  </p>
 
-                {/* --- Rating And Reviews */}
-                <RatingAndViews
-                  rating={singleProduct.rating}
-                  reviews={singleProduct.reviews}
-                />
-
-                {/* --- Price And Discount */}
-                <Discount
-                  price={singleProduct.price}
-                  discount={singleProduct.discount}
-                />
-
-                {/* Quantity */}
-                <div className="flex items-center gap-5 mt-5">
-                  <input
-                    type="number"
-                    onChange={(e) => setCount(Number(e.currentTarget.value))}
-                    className="border border-primary w-20 rounded-sm p-1"
-                    value={count}
-                    min={1}
-                    max={25}
+                  {/* --- Rating And Reviews */}
+                  <RatingAndViews
+                    rating={singleProduct.rating}
+                    reviews={singleProduct.reviews}
                   />
-                  <button
-                    type="button"
-                    aria-label="Add to cart"
-                    onClick={() => addToCartFunc(singleProduct)}
-                    className="bg-warning text-primary px-2 py-1 rounded-sm cursor-pointer hover:bg-amber-300 active:bg-warning active:scale-[0.98] transition-all duration-150"
-                  >
-                    Add to cart
-                  </button>
+
+                  {/* --- Price And Discount */}
+                  <Discount
+                    price={singleProduct.price}
+                    discount={singleProduct.discount}
+                  />
+
+                  {/* Quantity */}
+                  <div className="flex items-center gap-5 mt-5">
+                    <input
+                      type="number"
+                      onChange={(e) => setCount(Number(e.currentTarget.value))}
+                      className="border border-primary w-20 rounded-sm p-1"
+                      value={count}
+                      min={1}
+                      max={25}
+                    />
+                    <button
+                      type="button"
+                      aria-label="Add to cart"
+                      onClick={() => addToCartFunc(singleProduct)}
+                      className="bg-warning text-primary px-2 py-1 rounded-sm cursor-pointer hover:bg-amber-300 active:bg-warning active:scale-[0.98] transition-all duration-150"
+                    >
+                      Add to cart
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
+          </div>
+          <div>
+            {category === "laptops" && <Laptops excludeId={id} />}
+            {category === "mobiles" && <Mobiles excludeId={id} />}
+            {category === "special-offers" && (
+              <SpecialOffers className="mt-0" excludeId={id} />
+            )}
           </div>
         </div>
       )}
