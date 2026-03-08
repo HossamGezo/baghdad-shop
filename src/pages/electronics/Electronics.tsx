709fdf584e6d47fa497ac3013b09d488ecaf18c8
HossamGezo <ha2ghossam10@gmail.com>
2026-03-08 16:59:40 +0200

    refactor: migrate electronics page to use the new category template

--- src/pages/electronics/Electronics.tsx
+++ src/pages/electronics/Electronics.tsx
@@ -1,8 +1,31 @@
+// --- Libraries
+import { useEffect } from "react";
+
 // --- Local Components
-import CategoryTemplate from "@/components/category-template/CategoryTemplate";
+import CategoryTemplate from "@components/category-template/CategoryTemplate";
+
+// --- Custom Hooks & Actions
+import { useAppDispatch, useAppSelector } from "@app/hooks";
+import { fetchLaptops, fetchMobiles } from "@features/products/productsSlice";
 
 // --- Main Component
 const Electronics = () => {
+  // --- Fetching Data
+  const { loading, error, laptops, mobiles } = useAppSelector(
+    (state) => state.products,
+  );
+
+  // --- Dispatch Data
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (laptops.length === 0) {
+      dispatch(fetchLaptops());
+    }
+    if (mobiles.length === 0) {
+      dispatch(fetchMobiles());
+    }
+  }, [dispatch, laptops.length, mobiles.length]);
+
   return (
     <div>
       <CategoryTemplate
@@ -10,6 +33,9 @@ const Electronics = () => {
         altText={"Electronics Poster"}
         title="Electronics"
         currentCategoryKey="electronics"
+        loading={loading}
+        error={error}
+        items={[...laptops, ...mobiles]}
       />
     </div>
   );
