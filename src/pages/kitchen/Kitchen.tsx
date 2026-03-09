e3b909f5a0b3e88921d83b84b59ddf8f3fb8bb7d
HossamGezo <ha2ghossam10@gmail.com>
2026-03-09 16:04:41 +0200

    feat: complete kitchen category with dedicated appliances and cookware sections

--- src/pages/kitchen/Kitchen.tsx
+++ src/pages/kitchen/Kitchen.tsx
@@ -1,5 +1,43 @@
-const Kitchen = () => {
-  return <div>Kitchen Page</div>;
-};
+// --- Libraries
+import { useEffect } from "react";
 
+// --- Local Components
+import CategoryTemplate from "@components/category-template/CategoryTemplate";
+
+// --- Custom Hooks & Actions
+import { useAppDispatch, useAppSelector } from "@app/hooks";
+import { fetchProductsByCategory } from "@features/products/productsSlice";
+
+// --- Main Component
+const Kitchen = () => {
+  // --- Fetching Data
+  const { loading, error, appliances, cookware } = useAppSelector(
+    (state) => state.products,
+  );
+
+  // --- Dispatch Data
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (appliances.length === 0) {
+      dispatch(fetchProductsByCategory("appliances"));
+    }
+    if (cookware.length === 0) {
+      dispatch(fetchProductsByCategory("cookware"));
+    }
+  }, [appliances.length, cookware.length, dispatch]);
+
+  return (
+    <div>
+      <CategoryTemplate
+        imageSrc="/images/posters/kitchen-poster.png"
+        altText="Kitchen Poster"
+        title="Kitchen & Dining"
+        currentCategoryKey="kitchen"
+        loading={loading}
+        error={error}
+        items={[...appliances, ...cookware]}
+      />
+    </div>
+  );
+};
 export default Kitchen;
