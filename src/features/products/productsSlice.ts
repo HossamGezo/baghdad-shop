a9706c99eee82b32f40bf1d4095554cc9fc1fb7d
HossamGezo <ha2ghossam10@gmail.com>
2026-03-09 10:52:41 +0200

    feat: implement generic async thunk for dynamic category fetching

--- src/features/products/productsSlice.ts
+++ src/features/products/productsSlice.ts
@@ -1,8 +1,11 @@
 // --- Libraries
-import { createAsyncThunk, createSlice, isAnyOf } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 // --- Utils
 import api from "@utils/api";
+
+// --- Json
+import database from "~/@/db.json";
 
 // --- Types
 import type { ProductType } from "@/types";
@@ -20,6 +23,12 @@ type ProductsState = {
   laptops: ProductType[];
   mobiles: ProductType[];
   specialOffers: ProductType[];
+  appliances: ProductType[];
+  cookware: ProductType[];
+  clothing: ProductType[];
+  shoes: ProductType[];
+  dresses: ProductType[];
+  handbags: ProductType[];
   singleProduct: ProductType | null;
   error: string;
 };
@@ -30,65 +39,32 @@ const initialState: ProductsState = {
   laptops: [],
   mobiles: [],
   specialOffers: [],
+  appliances: [],
+  cookware: [],
+  clothing: [],
+  shoes: [],
+  dresses: [],
+  handbags: [],
   singleProduct: null,
   error: "",
 };
 
+// --- Generic Async Thunk Pattern Or Parametric Data Fetcher
 /**
- * @desc Get All Laptops
- * @route /laptops
+ * @desc Get All Products
+ * @route /:category
  * @method GET
  * @access public
  */
-export const fetchLaptops = createAsyncThunk<
-  ProductType[], // Return Type
-  void, // Argument Type
+type CategoryType = keyof typeof database;
+export const fetchProductsByCategory = createAsyncThunk<
+  { data: ProductType[]; category: string }, // Payload/Return Type
+  CategoryType, // Argument Type
   { rejectValue: string } // ThunkConfig
->("products/laptops", async (_, { rejectWithValue }) => {
+>("products/category", async (category, { rejectWithValue }) => {
   try {
-    const response = await api.get(`/laptops`);
-    const laptops = response.data;
-    return laptops;
-  } catch (error) {
-    return rejectWithValue(errorMsg(error));
-  }
-});
-
-/**
- * @desc Get All Mobiles
- * @route /mobiles
- * @method GET
- * @access public
- */
-export const fetchMobiles = createAsyncThunk<
-  ProductType[], // Return Type
-  void, // Argument Type
-  { rejectValue: string } // ThunkConfig
->("products/mobiles", async (_, { rejectWithValue }) => {
-  try {
-    const response = await api.get(`/mobiles`);
-    const mobiles = response.data;
-    return mobiles;
-  } catch (error) {
-    return rejectWithValue(errorMsg(error));
-  }
-});
-
-/**
- * @desc Get All Special Offers
- * @route /special-offers
- * @method GET
- * @access public
- */
-export const fetchSpecialOffers = createAsyncThunk<
-  ProductType[], // Return Type
-  void, // Argument Type
-  { rejectValue: string } // ThunkConfig
->("products/special-offers", async (_, { rejectWithValue }) => {
-  try {
-    const response = await api.get(`/special-offers`);
-    const specialOffers = response.data;
-    return specialOffers;
+    const response = await api.get(`/${category}`);
+    return { data: response.data, category };
   } catch (error) {
     return rejectWithValue(errorMsg(error));
   }
@@ -128,61 +104,29 @@ const productsSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    // ----- Fetch Laptops
-    builder.addCase(fetchLaptops.fulfilled, (state, action) => {
-      state.loading = false;
-      state.laptops = action.payload;
-      state.error = "";
-    });
-
-    // ----- Fetch Mobiles
-    builder.addCase(fetchMobiles.fulfilled, (state, action) => {
-      state.loading = false;
-      state.mobiles = action.payload;
-      state.error = "";
-    });
-
-    // --- Fetch Special Offers
-    builder.addCase(fetchSpecialOffers.fulfilled, (state, action) => {
-      state.loading = false;
-      state.specialOffers = action.payload;
-      state.error = "";
-    });
-
-    // --- Fetch Product By Id
-    builder.addCase(fetchSingleProduct.fulfilled, (state, action) => {
-      state.loading = false;
-      state.singleProduct = action.payload;
-      state.error = "";
-    });
-
     // ----- Pending Case
-    builder.addMatcher(
-      isAnyOf(
-        fetchLaptops.pending,
-        fetchMobiles.pending,
-        fetchSpecialOffers.pending,
-        fetchSingleProduct.pending,
-      ),
-      (state) => {
-        state.loading = true;
-        state.error = "";
-      },
-    );
+    builder.addCase(fetchProductsByCategory.pending, (state) => {
+      state.loading = true;
+      state.error = "";
+    });
+
+    // ----- Fulfilled Case
+    builder.addCase(fetchProductsByCategory.fulfilled, (state, action) => {
+      state.loading = false;
+
+      const { category, data } = action.payload;
+      const key = category as keyof ProductsState;
+      if (key !== "loading" && key !== "error" && key !== "singleProduct")
+        state[key] = data;
+
+      state.error = "";
+    });
 
     // ----- Rejected Case
-    builder.addMatcher(
-      isAnyOf(
-        fetchLaptops.rejected,
-        fetchMobiles.rejected,
-        fetchSpecialOffers.rejected,
-        fetchSingleProduct.rejected,
-      ),
-      (state, action) => {
-        state.loading = false;
-        state.error = action.payload || "An unexpected error occurred";
-      },
-    );
+    builder.addCase(fetchProductsByCategory.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload || "An unexpected error occurred";
+    });
   },
 });
 
