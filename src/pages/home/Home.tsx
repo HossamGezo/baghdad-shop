1a5db5e64f821aac3d967848922f89cc8cf52cd8
HossamGezo <ha2ghossam10@gmail.com>
2026-03-10 21:42:56 +0200

    refactor: eliminate redundant home sections and unify layout using the new generic components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,67 +1,17 @@
 // --- Local Components
-import CategoryPoster from "@components/category-poster/CategoryPoster";
 import HeroSection from "@pages/home/hero/HeroSection";
 import SpecialOffers from "@pages/home/special-offers/SpecialOffers";
-import Laptops from "@pages/home/laptops/Laptops";
-import Mobiles from "@pages/home/mobiles/Mobiles";
 import Brands from "@pages/home/brands/Brands";
-import Appliances from "@pages/home/appliances/Appliances";
-import Cookware from "@pages/home/cookware/Cookware";
-import Clothing from "@pages/home/clothing/Clothing";
-import Shoes from "@pages/home/shoes/Shoes";
-import Dresses from "@pages/home/dresses/Dresses";
-import Handbags from "@pages/home/handbags/Handbags";
+import Sections from "./sections/Sections";
 
 // --- Main Component
 const Home = () => {
-  const baseStyle =
-    "cursor-pointer hover:scale-[0.99] border border-transparent hover:border-warning transition-all duration-300";
   // --- Return JSX
   return (
     <>
       <HeroSection />
-      <SpecialOffers />
-
-      {/* Electronics */}
-      <CategoryPoster
-        imageSrc="/images/posters/electronics-poster.png"
-        altText="Electronics Poster"
-        to="/electronics"
-        className={baseStyle}
-      />
-      <Laptops />
-      <Mobiles />
-
-      {/* Kitchen */}
-      <CategoryPoster
-        imageSrc="/images/posters/kitchen-poster.png"
-        altText="Kitchen Poster"
-        to="/kitchen"
-        className={baseStyle}
-      />
-      <Appliances />
-      <Cookware />
-
-      {/* Men */}
-      <CategoryPoster
-        imageSrc="/images/posters/men-poster.png"
-        altText="Men Poster"
-        to="/men"
-        className={baseStyle}
-      />
-      <Clothing />
-      <Shoes />
-
-      {/* Women */}
-      <CategoryPoster
-        imageSrc="/images/posters/women-poster.png"
-        altText="Women Poster"
-        to="/women"
-        className={baseStyle}
-      />
-      <Dresses />
-      <Handbags />
-
+      <SpecialOffers id="special-offers" />
+      <Sections />
       <Brands />
     </>
   );
---
+++ src/pages/home/sections/Sections.tsx
@@ -0,0 +1,68 @@
+import CategoryPoster from "@components/category-poster/CategoryPoster";
+import HomeProductsSection from "@components/home-products-section/HomeProductsSection";
+
+const Sections = () => {
+  const categoryPosterStyle =
+    "cursor-pointer hover:scale-[0.99] border border-transparent hover:border-warning transition-all duration-300";
+
+  // --- Return JSX
+  return (
+    <div>
+      {/* 2. Kitchen Group */}
+      <CategoryPoster
+        imageSrc="/images/posters/kitchen-poster.png"
+        altText="Kitchen Poster"
+        to="/kitchen"
+        className={categoryPosterStyle}
+      />
+      <HomeProductsSection
+        title="Smart Home Appliances"
+        categoryKey="appliances"
+        to="/kitchen"
+      />
+      <HomeProductsSection
+        title="Professional Cookware Collection"
+        categoryKey="cookware"
+        to="/kitchen"
+      />
+
+      {/* 3. Men Fashion Group */}
+      <CategoryPoster
+        imageSrc="/images/posters/men-poster.png"
+        altText="Men Fashion Poster"
+        to="/men"
+        className={categoryPosterStyle}
+      />
+      <HomeProductsSection
+        title="Trendy Men's Clothing"
+        categoryKey="clothing"
+        to="/men"
+      />
+      <HomeProductsSection
+        title="Comfortable & Stylish Men's Shoes"
+        categoryKey="shoes"
+        to="/men"
+      />
+
+      {/* 4. Women Fashion Group */}
+      <CategoryPoster
+        imageSrc="/images/posters/women-poster.png"
+        altText="Women Collection Poster"
+        to="/women"
+        className={categoryPosterStyle}
+      />
+      <HomeProductsSection
+        title="Elegant Dresses for Every Occasion"
+        categoryKey="dresses"
+        to="/women"
+      />
+      <HomeProductsSection
+        title="Luxury Handbags & Accessories"
+        categoryKey="handbags"
+        to="/women"
+      />
+    </div>
+  );
+};
+
+export default Sections;
