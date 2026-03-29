3f2e8d80eb3976a256b2fb9de538bb7af3f54f13
HossamGezo <ha2ghossam10@gmail.com>
2026-03-29 15:06:41 +0200

    feat: enhance product card and button components with better types and props handling

--- src/components/card/ProductCard.tsx
+++ src/components/card/ProductCard.tsx
@@ -1,8 +1,8 @@
 // --- Libraries
-import { Link } from "react-router";
+import { Link, type LinkProps } from "react-router";
 
 // --- Types
-import type { ProductType } from "@/types";
+import type { ProductType } from "@/types/types";
 
 // --- Utils
 import { formatCurrency } from "@utils/formatCurrency";
@@ -12,33 +12,35 @@ import { cn } from "@utils/cn";
 import RatingAndViews from "@components/card/components/rating-and-reviews/RatingAndReviews";
 
 // --- Types
-type ProductCardProps = ProductType &
-  React.ComponentProps<"div"> & {
-    category: string;
-  };
+type ProductCardProps = Omit<LinkProps, "to"> & {
+  product: ProductType;
+};
 
 // --- Main Component
-const ProductCard = ({ className, ...product }: ProductCardProps) => {
+const ProductCard = ({ className, product, ...rest }: ProductCardProps) => {
+  const { id, category, firstImage, title, reviews, rating, price } = product;
+
   // --- Return JSX
   return (
     <Link
-      to={`/products/${product.category}/${product.id}`}
+      to={`/products/${category}/${id}`}
       className={cn(
         "bg-white min-w-70 sm:min-w-75 flex flex-col items-center p-4 shadow-soft rounded-md hover:scale-[1.03] hover:shadow-deep transition-all duration-300 cursor-pointer select-none",
         className,
       )}
+      {...rest}
     >
       <div className="w-40 h-40 mb-5">
         <img
-          src={product.firstImage}
-          alt={product.title}
+          src={firstImage}
+          alt={title}
           loading="lazy"
           className="w-full h-full object-contain"
         />
       </div>
-      <div className="font-medium line-clamp-1">{product.title}</div>
-      <RatingAndViews rating={product.rating} reviews={product.reviews} />
-      <div className="text-xl font-medium">{formatCurrency(product.price)}</div>
+      <div className="font-medium line-clamp-1">{title}</div>
+      <RatingAndViews rating={rating} reviews={reviews} />
+      <div className="text-xl font-medium">{formatCurrency(price)}</div>
     </Link>
   );
 };
