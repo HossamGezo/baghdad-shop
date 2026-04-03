873b201eb7c984beade5ec6e96b6c2cbf1eff7e1
HossamGezo <ha2ghossam10@gmail.com>
2026-04-03 21:32:51 +0200

    feat: implement product management with search, filtering, and responsive add/edit forms

--- src/pages/admin/AddProduct.tsx
+++ src/pages/admin/AddProduct.tsx
@@ -1,5 +1,228 @@
+// --- Libraries
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+// --- Components
+import InputField from "@components/inputs/InputField";
+import CustomButton from "@/components/custom-button/CustomButton";
+import ErrorHandler from "@components/error-handler/ErrorHandler";
+
+// --- Redux
+import { useAppDispatch, useAppSelector } from "@app/hooks";
+import { fetchProductsByCategory } from "@features/products/productsSlice";
+
+// --- Types
+import type { CategoriesType, ProductType } from "@/types/types";
+
+const categories = [
+  "laptops",
+  "mobiles",
+  "special-offers",
+  "appliances",
+  "cookware",
+  "clothing",
+  "shoes",
+  "dresses",
+  "handbags",
+];
+
+// --- Product Schema
+const ProductSchema = z.object({
+  title: z.string().trim(),
+  image: z.string().trim(),
+  price: z.string().trim(),
+  discount: z.string().trim(),
+  category: z.enum(categories),
+  description: z.string().trim(),
+});
+type ProductSchemaType = z.infer<typeof ProductSchema>;
+
+// --- Main Component
 const AddProduct = () => {
-  return <div>Add Product</div>;
+  // --- React Hook Form Logic
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<ProductSchemaType>({
+    mode: "onBlur",
+    resolver: zodResolver(ProductSchema),
+  });
+
+  // --- OnSubmit Function
+  const onSubmit: SubmitHandler<ProductSchemaType> = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  // --- Get Product Data from URL & Store
+  const { category, id } = useParams();
+
+  const dispatch = useAppDispatch();
+
+  const products: ProductType[] = useAppSelector((state) =>
+    category ? state.products[category as CategoriesType] : [],
+  );
+
+  // --- Fetch Product Category Data
+  useEffect(() => {
+    if (products.length == 0)
+      dispatch(fetchProductsByCategory(category as CategoriesType));
+  }, [dispatch, category, products.length]);
+
+  // --- Find Product
+  const product: ProductType | undefined = products.find(
+    (item) => item.id === id,
+  );
+
+  // --- Fill Form With Product Data
+  useEffect(() => {
+    if (product)
+      reset({
+        title: product?.title,
+        image: product?.firstImage,
+        price: String(product?.price),
+        discount: String(product?.discount),
+        category: product?.category,
+        description: product?.description,
+      });
+  }, [reset, product]);
+
+  // --- Return JSX
+  return (
+    <div className="flex flex-col h-full">
+      <h1 className="text-xl font-bold text-primary font-jetbrains bg-white p-5 rounded-md shadow-sm mb-5">
+        {product ? "Edit Product" : "Add New Product"}
+      </h1>
+      <div className="bg-white rounded-md shadow-sm h-full p-2.5">
+        {id && !product ? (
+          <div className="h-full flex items-center justify-center w-3/4 mx-auto">
+            <ErrorHandler error={"Product Not Found"} />
+          </div>
+        ) : (
+          <div className="bg-white p-3 rounded-xl mx-auto ">
+            <form
+              className="flex flex-col gap-5 rounded-lg"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
+                {/* Title */}
+                <InputField
+                  type="text"
+                  name="title"
+                  placeholder="Product title"
+                  register={register}
+                  label="Title"
+                  error={errors.title?.message}
+                  className="shadow-sm"
+                />
+
+                {/* Price */}
+                <InputField
+                  type="number"
+                  name="price"
+                  placeholder="Product price"
+                  register={register}
+                  label="Price"
+                  error={errors.price?.message}
+                  className="shadow-sm"
+                />
+
+                {/* (Change / Upload) Image */}
+                <div className="flex flex-col gap-2 lg:col-span-2 xl:col-span-1">
+                  <div className="w-full h-12.5 border border-gray-300 flex items-center overflow-hidden rounded-md shadow-sm hover:border-primary hover:ring-1 hover:ring-primary transition-[border-color,box-shadow] duration-500 ease-in-out cursor-pointer">
+                    <label
+                      htmlFor="image"
+                      className="bg-gray-300 h-full w-4/11 text-[12px] flex items-center justify-center font-jetbrains font-semibold cursor-pointer"
+                    >
+                      {product ? "Change Image" : "Upload Image"}
+                    </label>
+                    <input
+                      type="file"
+                      {...register("image")}
+                      name="image"
+                      id="image"
+                      className="w-7/11 bg-gray-300/50 h-full px-2.5 py-3.75 text-[13px] cursor-pointer"
+                    />
+                  </div>
+                  {product && (
+                    <div className="flex items-center gap-2 text-[10px] text-gray-500">
+                      <span className="font-bold">Current:</span>
+                      <img
+                        src={product.firstImage}
+                        alt="Current"
+                        className="w-8 h-8 object-contain border rounded"
+                      />
+                      <span className="truncate w-32">
+                        {product.firstImage}
+                      </span>
+                    </div>
+                  )}
+                </div>
+
+                {/* Description */}
+                <div className="lg:col-span-2 xl:col-span-3 relative flex flex-col gap-2.5">
+                  <label className="absolute bg-white text-primary/50 px-2.5 text-[12px] left-3.5 -top-2">
+                    Description
+                  </label>
+                  <textarea
+                    {...register("description")}
+                    placeholder="Product Description"
+                    className="w-full border border-gray-300 rounded-md p-2.5 focus:outline-none focus:border-primary h-32 pt-4"
+                  />
+                  {errors.description && (
+                    <span className="text-red-500 text-sm">
+                      {errors.description.message}
+                    </span>
+                  )}
+                </div>
+
+                {/* Discount */}
+                <div>
+                  <InputField
+                    type="number"
+                    name="discount"
+                    placeholder="Product discount"
+                    register={register}
+                    label="Discount"
+                    error={errors.discount?.message}
+                    className="shadow-sm"
+                  />
+                </div>
+
+                {/* Category */}
+                <select
+                  {...register("category")}
+                  name="category"
+                  id="category"
+                  className="border border-gray-300 h-12.5 rounded-md focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary transition-[border-color,box-shadow] duration-500 ease-in-out shadow-sm"
+                >
+                  {categories.map((cat, index) => (
+                    <option key={index} value={cat}>
+                      {cat}
+                    </option>
+                  ))}
+                </select>
+
+                {/* (Edit / Add) Button */}
+                <CustomButton
+                  type="submit"
+                  aria-label="Submit form"
+                  className="h-12.5 lg:col-span-2 xl:col-span-1"
+                >
+                  {product ? "Edit" : "Add"}
+                </CustomButton>
+              </div>
+            </form>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AddProduct;
--- src/pages/admin/ProductsList.tsx
+++ src/pages/admin/ProductsList.tsx
@@ -1,5 +1,255 @@
+// --- Libraries
+import { useState, useMemo, useEffect } from "react";
+import { Link, useNavigate } from "react-router";
+
+// --- React Icons
+import { FiSearch, FiEdit, FiTrash2, FiPlus } from "react-icons/fi";
+
+// --- Utils
+import { formatCurrency } from "@utils/formatCurrency";
+import { cn } from "@utils/cn";
+
+// --- Custom Hooks & Actions
+import { useAppDispatch, useAppSelector } from "@app/hooks";
+import { fetchProductsByCategory } from "@features/products/productsSlice";
+
+// --- Local Components
+import CustomButton from "@components/custom-button/CustomButton";
+import Modal from "@components/modal/Modal";
+
+// --- Types
+import type { CategoriesType, ProductType } from "@/types/types";
+
+// --- Main Component
 const ProductsList = () => {
-  return <div>Products List</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const state = useAppSelector((state) => state.products);
+  const dispatch = useAppDispatch();
+
+  // --- Logic: Dispatch all empty categories
+  useEffect(() => {
+    const categories: CategoriesType[] = [
+      "laptops",
+      "mobiles",
+      "specialOffers",
+      "appliances",
+      "cookware",
+      "clothing",
+      "shoes",
+      "dresses",
+      "handbags",
+    ];
+
+    categories.forEach((cat) => {
+      const currentCategory = state[cat];
+
+      if (currentCategory.length === 0) {
+        dispatch(fetchProductsByCategory(cat));
+      }
+    });
+  }, [state, dispatch]);
+
+  const allProducts = useMemo(() => {
+    return [
+      ...state.laptops,
+      ...state.mobiles,
+      ...state.specialOffers,
+      ...state.appliances,
+      ...state.cookware,
+      ...state.clothing,
+      ...state.shoes,
+      ...state.dresses,
+      ...state.handbags,
+    ];
+  }, [
+    state.laptops,
+    state.mobiles,
+    state.specialOffers,
+    state.appliances,
+    state.cookware,
+    state.clothing,
+    state.shoes,
+    state.dresses,
+    state.handbags,
+  ]);
+
+  // --- Logic: Filtering
+  const filteredProducts = useMemo(() => {
+    return allProducts.filter(
+      (product) =>
+        product.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        product.category.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+  }, [allProducts, searchTerm]);
+
+  // --- Handle Modal
+  const [isOpen, setIsOpen] = useState(false);
+  const [productToDelete, setProductToDelete] = useState<ProductType | null>(
+    null,
+  );
+
+  const onConfirm = () => {
+    if (productToDelete) {
+      console.log(`" ${productToDelete.title} " Has been deleted successfully`);
+    }
+
+    // Delete Logic
+
+    setProductToDelete(null);
+  };
+
+  // --- Return JSX
+  return (
+    <div className="flex flex-col h-full">
+      {/* --- Header --- */}
+      <div className="bg-white p-5 rounded-md shadow-sm mb-5 flex flex-col xl:flex-row justify-between items-start xl:items-center gap-4">
+        <div>
+          <h1 className="text-xl font-bold text-primary font-jetbrains">
+            Inventory Management
+          </h1>
+          <p className="text-sm text-gray-500">
+            Total Products in store: ({filteredProducts.length})
+          </p>
+        </div>
+
+        <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center gap-3 w-full xl:w-auto">
+          {/* Search Input */}
+          <div className="relative w-full lg:w-72">
+            <FiSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search products or category..."
+              className="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary text-sm transition-[border-color,box-shadow] duration-500 ease-in-out"
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+            />
+          </div>
+
+          {/* Add Product Button */}
+          <CustomButton
+            type="button"
+            className="text-primary px-4 py-2 rounded-md font-semibold flex items-center justify-center gap-2"
+            onClick={() => navigate("/admin/add-product")}
+          >
+            <FiPlus /> Add Product
+          </CustomButton>
+        </div>
+      </div>
+
+      {/* --- Table --- */}
+      <div className="flex-1 bg-white rounded-md shadow-sm overflow-hidden flex flex-col">
+        <div className="overflow-x-auto custom-scrollbar flex-1">
+          <table
+            className={cn(
+              "w-full min-w-max relative border-separate border-spacing-0",
+              filteredProducts.length === 0 && "h-full",
+            )}
+          >
+            <thead>
+              <tr className="sticky top-0 z-10 bg-[#EFF2F3] shadow-sm text-neutral-700 *:whitespace-nowrap *:text-center *:text-[10px] *:px-3 *:py-2.5 *:flex-1 *:select-none *:not-last:border-r *:border-r-warning">
+                <th>IMAGE</th>
+                <th>PRODUCT NAME</th>
+                <th>CATEGORY</th>
+                <th>PRICE</th>
+                <th>RATING</th>
+                <th>ACTIONS</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredProducts.length > 0 ? (
+                filteredProducts.map((product) => (
+                  <tr
+                    key={product.id}
+                    className="even:bg-[#EFF2F3]/75 hover:bg-gray-500/10 transition-colors duration-200 *:whitespace-nowrap *:px-3 *:py-2.5 *:text-center *:text-[12px] *:select-none"
+                  >
+                    {/* Image */}
+                    <td>
+                      <div className="bg-white p-1 rounded border border-gray-100 w-12 h-12 mx-auto">
+                        <img
+                          src={product.firstImage}
+                          alt={product.title}
+                          loading="lazy"
+                          className="w-full h-full object-contain"
+                        />
+                      </div>
+                    </td>
+
+                    {/* Name */}
+                    <td className="w-25">
+                      <p
+                        className="font-semibold text-primary text-[13px] line-clamp-1"
+                        title={product.title}
+                      >
+                        {product.title}
+                      </p>
+                    </td>
+
+                    {/* Category */}
+                    <td>
+                      <span className="px-2 py-0.5 bg-gray-100 text-gray-600 rounded text-[10px] border border-gray-200 uppercase">
+                        {product.category}
+                      </span>
+                    </td>
+
+                    {/* Price */}
+                    <td className="font-bold text-primary">
+                      {formatCurrency(product.price)}
+                    </td>
+
+                    {/* Rating */}
+                    <td>
+                      <div className="flex items-center justify-center gap-1">
+                        <span className="text-amber-500">★</span>
+                        <span>{product.rating}</span>
+                      </div>
+                    </td>
+
+                    {/* Actions */}
+                    <td>
+                      <div className="flex items-center justify-center gap-4">
+                        <Link
+                          to={`/admin/products/edit/${product.category}/${product.id}`}
+                          className="text-blue-500 hover:text-blue-700 transition-colors p-1 hover:bg-blue-50 rounded"
+                          title="Edit Product"
+                        >
+                          <FiEdit size={18} />
+                        </Link>
+                        <button
+                          type="button"
+                          className="text-red-500 hover:text-red-700 transition-colors p-1 hover:bg-red-50 rounded cursor-pointer"
+                          title="Delete Product"
+                          onClick={() => {
+                            setIsOpen(true);
+                            setProductToDelete(product);
+                          }}
+                        >
+                          <FiTrash2 size={18} />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td
+                    colSpan={7}
+                    className="text-center text-gray-500/75 tracking-wide font-jetbrains text-xl select-none"
+                  >
+                    No products found matching your search criteria
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {isOpen && <Modal setIsOpen={setIsOpen} onConfirm={onConfirm} />}
+    </div>
+  );
 };
 
 export default ProductsList;
