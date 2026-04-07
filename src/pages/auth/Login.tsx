b8b39f852bd27bc396ef29c36c777fb1c9105dee
HossamGezo <ha2ghossam10@gmail.com>
2026-04-07 20:24:27 +0200

    feat: connect login, register, and reset password pages to auth logic and navigation

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,59 +1,57 @@
 // --- Libraries
+import { Link } from "react-router";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { Link } from "react-router";
+
+// --- RTK
+import { useAppDispatch, useAppSelector } from "@app/hooks";
+import { loginUser } from "@features/auth/authSlice";
 
 // --- Components
 import InputField from "@components/inputs/InputField";
 import PasswordField from "@components/inputs/PasswordField";
-import CustomButton from "@/components/custom-button/CustomButton";
+import CustomButton from "@components/custom-button/CustomButton";
 
 // --- Register Schema
 const LoginSchema = z.object({
   email: z.string().email({ message: "Invalid Email Address" }).trim(),
-  password: z
-    .string()
-    .trim()
-    .min(8, { message: "Password must be at least 8 characters" }),
+  password: z.string().trim().min(8, { message: "Password must be at least 8 characters" }),
 });
 type LoginSchemaType = z.infer<typeof LoginSchema>;
 
 // --- Main Component
 const Login = () => {
+  // --- Custom RTK Hooks
+  const { loading } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+
   // --- React Hook Form Logic
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
   } = useForm<LoginSchemaType>({
     mode: "onBlur",
     resolver: zodResolver(LoginSchema),
   });
+
   // --- OnSubmit Function
   const onSubmit: SubmitHandler<LoginSchemaType> = (data) => {
-    console.log(data);
-    reset();
+    dispatch(loginUser(data));
   };
 
   // --- Return JSX
   return (
     <div className="custom-container flex items-center justify-center flex-col gap-5 py-5 flex-1">
       <h1 className="text-primary mb-2.5 font-jetbrains text-center flex flex-col">
-        <span className="max-sm:text-2xl text-3xl font-bold mb-2.5">
-          Welcome back
-        </span>
-        <p className="max-sm:text-[12px] text-[14px] font-light">
-          Log in to continue your shopping experience
-        </p>
+        <span className="max-sm:text-2xl text-3xl font-bold mb-2.5">Welcome back</span>
+        <p className="max-sm:text-[12px] text-[14px] font-light">Log in to continue your shopping experience</p>
       </h1>
-      <div className="bg-white rounded-xl max-sm:w-77.5">
-        <h2 className="text-2xl w-fit mx-auto text-primary mt-5 mb-2.5 font-jetbrains">
-          Login
-        </h2>
+      <div className="bg-white rounded-xl">
+        <h2 className="text-2xl w-fit mx-auto text-primary mt-5 mb-2.5 font-jetbrains">Login</h2>
         <form
-          className="shadow-lg flex flex-col gap-5 max-sm:p-5 sm:px-10 sm:pb-10 rounded-lg"
+          className="shadow-lg flex flex-col gap-5 max-sm:p-5 sm:px-10 sm:pb-10 rounded-lg min-w-86"
           onSubmit={handleSubmit(onSubmit)}
         >
           <InputField
@@ -74,13 +72,10 @@ const Login = () => {
             error={errors.password?.message}
             autoComplete="current-password"
           />
-          <Link
-            to="/reset-password"
-            className="-mt-2.5 text-[14px] text-blue-600/70 hover:text-blue-600"
-          >
+          <Link to="/reset-password" className="-mt-2.5 text-[14px] text-blue-600/70 hover:text-blue-600">
             Forgot Password?
           </Link>
-          <CustomButton type="submit" aria-label="Submit form">
+          <CustomButton type="submit" aria-label="Submit form" isLoading={loading}>
             Login
           </CustomButton>
           <button
@@ -88,21 +83,14 @@ const Login = () => {
             aria-label="Login with Google"
             className="border border-primary/25 text-primary py-1.5 rounded-md cursor-pointer flex items-center justify-center gap-2.5 transition-colors duration-200 hover:border-t-[#FF3D00] hover:border-r-[#1B76D2] hover:border-b-[#4CAF51] hover:border-l-[#FFC106] shadow-standard"
           >
-            <img
-              src="/images/icons/google-icon.svg"
-              alt="Google Icon"
-              className="w-6"
-            />
+            <img src="/images/icons/google-icon.svg" alt="Google Icon" className="w-6" />
             Login with Google
           </button>
         </form>
       </div>
       <p className="text-primary/75 select-none">
         Don't have an account?
-        <Link
-          to="/register"
-          className="text-blue-600/70 hover:text-blue-600 ml-0.5 cursor-pointer"
-        >
+        <Link to="/register" className="text-blue-600/70 hover:text-blue-600 ml-0.5 cursor-pointer">
           Register
         </Link>
       </p>
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -4,25 +4,26 @@ import { useForm, type SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
+// --- RTK
+import { useAppDispatch, useAppSelector } from "@app/hooks";
+import { registerUser } from "@features/auth/authSlice";
+
 // --- Components
 import InputField from "@components/inputs/InputField";
 import PasswordField from "@components/inputs/PasswordField";
-import CustomButton from "@/components/custom-button/CustomButton";
+import CustomButton from "@components/custom-button/CustomButton";
 
 // --- Register Schema
 const RegisterSchema = z
   .object({
-    username: z
+    fullName: z
       .string()
       .trim()
-      .min(3, { message: "Username must be at least 3 characters" })
-      .max(21, { message: "Username must not exceed 21 characters" }),
+      .min(3, { message: "Full Name must be at least 3 characters" })
+      .max(21, { message: "Full Name must not exceed 21 characters" }),
     email: z.string().email({ message: "Invalid Email Address" }).trim(),
-    password: z
-      .string()
-      .trim()
-      .min(8, { message: "Password must be at least 8 characters" }),
-    confirmPassword: z.string(),
+    password: z.string().trim().min(8, { message: "Password must be at least 8 characters" }),
+    confirmPassword: z.string().trim().min(8),
   })
   .refine((data) => data.password === data.confirmPassword, {
     path: ["confirmPassword"],
@@ -32,50 +33,47 @@ type RegisterSchemaType = z.infer<typeof RegisterSchema>;
 
 // --- Main Component
 const Register = () => {
+  // --- Custom RTK Hooks
+  const { loading } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+
   // --- React Hook Form Logic
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
   } = useForm<RegisterSchemaType>({
     mode: "onBlur",
     resolver: zodResolver(RegisterSchema),
   });
+
   // --- OnSubmit Function
   const onSubmit: SubmitHandler<RegisterSchemaType> = (data) => {
-    console.log(data);
-    reset();
+    dispatch(registerUser(data));
   };
 
   // --- Return JSX
   return (
     <div className="custom-container flex items-center justify-center flex-col gap-5 py-5 flex-1">
       <h1 className="text-primary mb-2.5 font-jetbrains text-center flex flex-col">
-        <span className="max-sm:text-2xl text-3xl font-bold mb-2.5">
-          Create New Account
-        </span>
-        <p className="max-sm:text-[12px] text-[14px] font-light">
-          Sign up to start your shopping journey
-        </p>
+        <span className="max-sm:text-2xl text-3xl font-bold mb-2.5">Create New Account</span>
+        <p className="max-sm:text-[12px] text-[14px] font-light">Sign up to start your shopping journey</p>
       </h1>
-      <div className="bg-white rounded-xl max-sm:w-77.5">
-        <h2 className="text-2xl w-fit mx-auto text-primary mt-5 mb-2.5 font-jetbrains">
-          Register
-        </h2>
+      <div className="bg-white rounded-xl">
+        <h2 className="text-2xl w-fit mx-auto text-primary mt-5 mb-2.5 font-jetbrains">Register</h2>
         <form
-          className="shadow-lg flex flex-col gap-5 max-sm:p-5 sm:px-10 sm:pb-10 rounded-lg"
+          className="shadow-lg flex flex-col gap-5 max-sm:p-5 sm:px-10 sm:pb-10 rounded-lg min-w-86"
           onSubmit={handleSubmit(onSubmit)}
         >
           <InputField
             autoFocus
             type="text"
-            placeholder="Enter your username"
-            name="username"
-            label="Username"
+            placeholder="Enter your Full Name"
+            name="fullName"
+            label="Full Name"
             register={register}
-            error={errors.username?.message}
-            autoComplete="username"
+            error={errors.fullName?.message}
+            autoComplete="name"
           />
           <InputField
             type="email"
@@ -104,7 +102,7 @@ const Register = () => {
             error={errors.confirmPassword?.message}
             autoComplete="new-password"
           />
-          <CustomButton type="submit" aria-label="Submit form">
+          <CustomButton type="submit" aria-label="Submit form" isLoading={loading}>
             Register
           </CustomButton>
           <button
@@ -112,21 +110,14 @@ const Register = () => {
             aria-label="Continue with Google"
             className="border border-primary/25 text-primary py-1.5 rounded-md cursor-pointer flex items-center justify-center gap-2.5 transition-colors duration-200 hover:border-t-[#FF3D00] hover:border-r-[#1B76D2] hover:border-b-[#4CAF51] hover:border-l-[#FFC106] shadow-standard"
           >
-            <img
-              src="/images/icons/google-icon.svg"
-              alt="Google Icon"
-              className="w-6"
-            />
+            <img src="/images/icons/google-icon.svg" alt="Google Icon" className="w-6" />
             Continue with Google
           </button>
         </form>
       </div>
       <p className="text-primary/75 select-none">
         Already have an account?
-        <Link
-          to="/login"
-          className="text-blue-600/70 hover:text-blue-600 ml-0.5 cursor-pointer"
-        >
+        <Link to="/login" className="text-blue-600/70 hover:text-blue-600 ml-0.5 cursor-pointer">
           Login
         </Link>
       </p>
