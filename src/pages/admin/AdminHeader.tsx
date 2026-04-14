9cc08721515179e7c74574fd9882368f034c250c
HossamGezo <ha2ghossam10@gmail.com>
2026-04-14 08:21:02 +0200

    feat: implement dynamic profile dropdown and avatar fallback in admin header

--- src/pages/admin/AdminHeader.tsx
+++ src/pages/admin/AdminHeader.tsx
@@ -1,9 +1,20 @@
+// --- React
+import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router";
+
 // --- React Icons
 import { IoIosNotificationsOutline } from "react-icons/io";
-import { MdMenuOpen, MdOutlineMenu } from "react-icons/md";
+import { MdMenuOpen, MdOutlineDashboard, MdOutlineMenu } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { BiBasket } from "react-icons/bi";
 
 // --- Local Components
 import Logo from "@components/logo/Logo";
+import CustomButton from "@components/custom-button/CustomButton";
+
+// --- RTK
+import { useAppDispatch, useAppSelector } from "@app/hooks";
+import { logout } from "@features/auth/authSlice";
 
 // --- Utils
 import { cn } from "@utils/cn";
@@ -19,6 +30,45 @@ type SidebarTypeProps = {
 
 // --- Main Component
 const AdminHeader = ({ isOpen, setIsOpen }: SidebarTypeProps) => {
+  // --- Is Authenticated
+  const { user } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+
+  // --- Drop Down Menu
+  const [openDropMenu, setOpenDropMenu] = useState<boolean>(false);
+
+  // --- Handle Drop Down Menu Button
+  const handleClick = (e: React.MouseEvent<HTMLImageElement>) => {
+    e.stopPropagation();
+    setOpenDropMenu((prev) => !prev);
+  };
+
+  // --- Handle Drop Down Menu Button "Click Outside Dropdown Menu"
+  const dropDownRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (dropDownRef.current && !dropDownRef.current.contains(e.target as Node)) {
+        setOpenDropMenu(false);
+      }
+    };
+
+    if (openDropMenu) {
+      window.addEventListener("click", handleOutsideClick);
+    }
+
+    return () => {
+      window.removeEventListener("click", handleOutsideClick);
+    };
+  }, [openDropMenu, setOpenDropMenu]);
+
+  // --- Handle Logout
+  const handleLogout = () => {
+    dispatch(logout());
+    setOpenDropMenu(false);
+  };
+
+  // --- Return JSX
   return (
     <header className={cn("flex bg-primary py-3")}>
       {/* Left Hand Sied */}
@@ -64,16 +114,62 @@ const AdminHeader = ({ isOpen, setIsOpen }: SidebarTypeProps) => {
           >
             <IoIosNotificationsOutline size={27} />
           </button>
-          <div className="flex items-center gap-2.5 border-l border-l-[#d5d5d5]/25 pl-5 max-sm:pl-2.5">
+          <div
+            className="relative flex items-center gap-2.5 border-l border-l-[#d5d5d5]/25 pl-5 max-sm:pl-2.5"
+            ref={dropDownRef}
+          >
             <img
-              src="/images/avatar/avatar1.png"
-              alt="Avatar"
-              className="w-12 max-sm:w-10 h-12 max-sm:h-10 object-contain rounded-full p-0.5 cursor-pointer border border-amber-400 hover:shadow-md transition-shadow duration-300 shadow-amber-500/25"
+              src={user?.avatar}
+              alt={user?.fullName}
+              onClick={(e) => handleClick(e)}
+              loading="lazy"
+              className={cn(
+                "w-12 max-sm:w-10 h-12 max-sm:h-10 object-contain rounded-full p-0.5 cursor-pointer border border-amber-400 hover:shadow-md transition-shadow duration-300 shadow-amber-500/25 text-white",
+                openDropMenu && "shadow-md",
+              )}
+              onError={(e) => {
+                (e.target as HTMLImageElement).src =
+                  `https://ui-avatars.com/api/?name=${user?.fullName}&background=0f172a&color=f87171`;
+              }}
             />
             <div className="text-white flex flex-col">
-              <span className="max-sm:text-[11px]">Hossam Gouda</span>
+              <span className="max-sm:text-[11px]">{user?.fullName}</span>
               <span className="max-sm:text-[9px] text-[11px] text-[#d5d5d5]">@Baghdadshop</span>
             </div>
+
+            {/* Drop Down Menu */}
+            {openDropMenu && (
+              <div className="absolute flex flex-col top-13 right-2.5 rounded-md bg-white shadow-sm w-50 h-fit overflow-hidden z-10000">
+                <div className="flex flex-col gap-2.5">
+                  {/* Only for admin */}
+                  <Link
+                    to="/admin"
+                    onClick={() => setOpenDropMenu(false)}
+                    className="text-primary p-2.5 border-b border-gray-400 flex items-center gap-2.5 font-semibold hover:bg-gray-300 font-jetbrains tracking-tighter"
+                  >
+                    <MdOutlineDashboard size={17} /> Dashboard
+                  </Link>
+                  <Link
+                    to="/profile"
+                    onClick={() => setOpenDropMenu(false)}
+                    className="text-primary p-2.5 border-b border-gray-400 flex items-center gap-2.5 font-semibold hover:bg-gray-300 font-jetbrains tracking-tighter"
+                  >
+                    <FaUser size={17} /> My Profile
+                  </Link>
+                  <Link
+                    to="/profile/orders"
+                    onClick={() => setOpenDropMenu(false)}
+                    className="text-primary p-2.5 border-b border-gray-400 flex items-center gap-2.5 font-semibold hover:bg-gray-300 font-jetbrains tracking-tighter"
+                  >
+                    <BiBasket size={17} /> My Orders
+                  </Link>
+                </div>
+
+                <CustomButton type="button" onClick={handleLogout} className="w-37.5 mx-auto mt-auto my-5">
+                  Logout
+                </CustomButton>
+              </div>
+            )}
           </div>
         </div>
       </div>
