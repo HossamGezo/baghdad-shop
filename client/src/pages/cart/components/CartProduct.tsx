002d03e6d3682a34842998b8ae6fbb3860f4a12d
HossamGezo <ha2ghossam10@gmail.com>
2026-04-16 15:52:52 +0200

    refactor(client): cleanup cart product UI and improve responsiveness

--- client/src/pages/cart/components/CartProduct.tsx
+++ client/src/pages/cart/components/CartProduct.tsx
@@ -40,9 +40,9 @@ const CartProduct = (product: CartType) => {
 
   //--- Return JSX
   return (
-    <div className="relative flex max-sm:flex-col max-sm:items justify-between border-b-2 border-b-body last:border-b-0 w-[97.5%] mx-auto p-3">
+    <div className="relative flex justify-between border-b-2 border-b-body last:border-b-0 w-[97.5%] mx-auto p-3">
       {/* Cart Product Left */}
-      <div className="flex flex-col gap-2.5 w-3/4 max-xl:w-3/5">
+      <div className="flex flex-col gap-5 w-3/4 max-xl:w-3/5">
         {/* ---- Cart Left Details */}
         <Link to={`/products/${product.category}/${product.id}`}>
           <div className="flex items-center gap-2.5">
@@ -64,7 +64,7 @@ const CartProduct = (product: CartType) => {
           type="button"
           aria-label="Remove from cart"
           onClick={() => removeFromCartFunc(product.id)}
-          className="max-sm:absolute bottom-6 cart-left-remove text-warning flex items-center gap-1 font-medium cursor-pointer hover:bg-amber-400 hover:text-white w-fit p-1 pr-1.5 rounded-sm transition-colors duration-150"
+          className="text-warning flex items-center gap-1 font-medium cursor-pointer hover:bg-amber-400 hover:text-white w-fit p-1 pr-1.5 rounded-sm transition-colors duration-150"
         >
           <MdOutlineDeleteOutline className="text-xl" />
           Remove
@@ -98,7 +98,7 @@ const CartProduct = (product: CartType) => {
           >
             <HiMinus className="max-sm:text-[12px]" />
           </button>
-          <span className="text-lg font-medium w-5 text-center">{product.count}</span>
+          <span className="text-lg font-medium w-15 text-center border">{product.count}</span>
           <button
             type="button"
             aria-label="Increase Quantity by 1"
