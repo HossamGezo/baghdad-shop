facf0a9ef788b281f885a5fca0b0e953dc0c4e2a
HossamGezo <ha2ghossam10@gmail.com>
2026-04-28 19:36:45 +0300

    refactor(server): implement type-safe error handler with conditional stack trace

--- server/src/shared/middlewares/error.middleware.ts
+++ server/src/shared/middlewares/error.middleware.ts
@@ -14,7 +14,18 @@ export const notFound = (req: Request, res: Response, next: NextFunction) => {
 export const errorHandler = (error: unknown, _req: Request, res: Response, _next: NextFunction) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 
-  const message = error instanceof Error ? error.message : "Something went Wrong!";
+  let message = "Something went wrong!";
+  let stack = null;
 
-  res.status(statusCode).json({ message });
+  if (error instanceof Error) {
+    message = error.message;
+    stack = error.stack;
+  } else if (typeof error === "string") {
+    message = error;
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? null : stack,
+  });
 };
