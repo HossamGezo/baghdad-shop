d33b64690df451a72a729bef234f839bcd506bfa
HossamGezo <ha2ghossam10@gmail.com>
2026-05-07 17:30:09 +0300

    refactor(auth): connect forgot password page to backend and improve UX with redirect logic

--- client/src/pages/auth/ResetPassword.tsx
+++ client/src/pages/auth/ResetPassword.tsx
@@ -1,29 +1,29 @@
 // --- Libraries
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import toast from "react-hot-toast";
 
 // --- RTK
 import { useAppDispatch, useAppSelector } from "@app/hooks";
-import { resetPassword } from "@features/auth/authSlice";
+import { sendResetPasswordLink } from "@features/auth/authSlice";
 
 // --- Components
 import InputField from "@components/inputs/InputField";
 import CustomButton from "@components/custom-button/CustomButton";
 
-// --- Register Schema
+// --- Reset Password Schema
 const ResetPasswordSchema = z.object({
   email: z.string().email({ message: "Invalid Email Address" }).trim(),
 });
-
 type ResetPasswordSchemaType = z.infer<typeof ResetPasswordSchema>;
 
 // --- Main Component
 const ResetPassword = () => {
-  // --- Custom RTK Hooks
   const { loading } = useAppSelector((state) => state.auth);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   // --- React Hook Form Logic
   const {
@@ -37,20 +37,31 @@ const ResetPassword = () => {
   });
 
   // --- OnSubmit Function
-  const onSubmit: SubmitHandler<ResetPasswordSchemaType> = (data) => {
-    dispatch(resetPassword(data));
-    reset();
+  const onSubmit: SubmitHandler<ResetPasswordSchemaType> = async (data) => {
+    const resultAction = await dispatch(sendResetPasswordLink(data));
+
+    if (sendResetPasswordLink.fulfilled.match(resultAction)) {
+      toast.success(resultAction.payload as string, { duration: 5000 });
+      reset();
+
+      setTimeout(() => {
+        navigate("/login");
+      }, 3000);
+    }
   };
 
   // --- Return JSX
   return (
     <div className="custom-container flex items-center justify-center flex-col gap-5 py-5 flex-1">
       <h1 className="text-primary mb-2.5 font-jetbrains text-center flex flex-col">
-        <span className="max-sm:text-2xl text-3xl font-bold mb-2.5">Reset Password</span>
-        <p className="max-sm:text-[12px] text-[14px] font-light">Enter your email to receive a password reset link</p>
+        <span className="max-sm:text-2xl text-3xl font-bold mb-2.5">Forgot Password?</span>
+        <p className="max-sm:text-[12px] text-[14px] font-light">
+          No worries! Enter your email to receive a secure reset link.
+        </p>
       </h1>
+
       <div className="bg-white rounded-xl">
-        <h2 className="text-2xl w-fit mx-auto text-primary mt-5 mb-2.5 font-jetbrains">Reset Password</h2>
+        <h2 className="text-2xl w-fit mx-auto text-primary mt-5 mb-2.5 font-jetbrains">Reset Request</h2>
         <form
           className="shadow-lg flex flex-col gap-4 max-sm:p-5 sm:px-10 sm:pb-10 rounded-lg min-w-80 sm:min-w-88"
           onSubmit={handleSubmit(onSubmit)}
@@ -58,24 +69,26 @@ const ResetPassword = () => {
           <InputField
             type="email"
             name="email"
-            placeholder="Enter your email"
+            placeholder="Enter your registered email"
             register={register}
             label="Email"
             error={errors.email?.message}
             autoComplete="email"
           />
-          <CustomButton type="submit" aria-label="Submit form" isLoading={loading}>
-            Submit
+          <CustomButton type="submit" aria-label="Send Reset Link" isLoading={loading}>
+            Send Link
           </CustomButton>
         </form>
       </div>
+
       <p className="text-primary/75 select-none">
-        Back to
+        Remember your password?
         <Link to="/login" className="text-blue-600/70 hover:text-blue-600 ml-1.5 cursor-pointer">
-          Login
+          Back to Login
         </Link>
       </p>
     </div>
   );
 };
+
 export default ResetPassword;
